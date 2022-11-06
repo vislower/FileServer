48389341fd2ed30724c019a2dff2382c104b4d40
vislower <115408772+vislower@users.noreply.github.com>
2022-11-06 18:21:00 +0100

    Handled exceptions for SocketIO.java

--- src/main/java/org/vislower/fileserver/SocketIO.java
+++ src/main/java/org/vislower/fileserver/SocketIO.java
@@ -7,11 +7,21 @@ import java.net.Socket;
 
 public class SocketIO {
 
-    public static DataInputStream createInputStream(Socket socket) throws IOException {
-        return new DataInputStream(socket.getInputStream());
+    public static DataInputStream createInputStream(Socket socket){
+        try {
+            return new DataInputStream(socket.getInputStream());
+        }catch (IOException e) {
+            System.out.println("ERROR : socket is closed/not connected or an I/O error occured when creating the input stream");
+        }
+        return null;
     }
 
-    public static DataOutputStream createOutputStream(Socket socket) throws IOException {
-        return new DataOutputStream(socket.getOutputStream());
+    public static DataOutputStream createOutputStream(Socket socket){
+        try {
+            return new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            System.out.println("ERROR : socket is closed/not connected or an I/O error occured when creating the input stream");
+        }
+        return null;
     }
 }
